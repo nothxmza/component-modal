449cd8f26feabc65a8b168d7f4115699601ddecb
nothxmza <terras.hamza@hotmail.fr>
2025-07-10 16:38:39 +0200

    .

--- src/Exemple.jsx
+++ src/Exemple.jsx
@@ -1,5 +1,5 @@
-import { Modal } from "./Modal";
 import { useState } from "react";
+import { Modal } from "./Modal";
 
 export const Exemple = () => {
 	  const [isOpen, setIsOpen] = useState(false);
